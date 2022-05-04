3484d6ead4ad6a1e9094151763ec9ad38ec98561
Bruma Sebastian <SebastianBruma24@gmail.com>
2022-05-04 13:31:45 +0300

    Add project files.

---
+++ FormInitializare.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Proiect_BD_SituatieScolara
+{
+    public partial class FormInitializare : Form
+    {
+        public FormInitializare()
+        {
+            InitializeComponent();
+        }
+    }
+}
