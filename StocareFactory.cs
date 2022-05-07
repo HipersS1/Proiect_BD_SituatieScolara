444d4616468b3f3fb9cfd377318622e5297469f9
Bruma Sebastian <SebastianBruma24@gmail.com>
2022-05-07 21:40:10 +0300

    Introduction of classes

---
+++ StocareFactory.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NivelAccesDate;
+
+namespace Proiect_BD_SituatieScolara
+{
+    public class StocareFactory
+    {
+        public IStocareFactory GetTipStocare(Type tipEntitate)
+        {
+            var formatSalvare = ConfigurationManager.AppSettings["FormatSalvare"];
+            if (formatSalvare != null)
+            {
+                switch (formatSalvare)
+                {
+                    default:
+                    case "BazaDateOracle":
+
+                        //if (tipEntitate == typeof(Companie))
+                        //{
+                        //    return new AdministrareCompanii();
+                        //}
+                        //if (tipEntitate == typeof(Masina))
+                        //{
+                        //    return new AdministrareMasini();
+                        //}
+                        //break;
+
+                    case "BIN":
+                        //instantiere clase care realizeaza salvarea in fisier binar
+                        break;
+                }
+            }
+            return null;
+        }
+    }
+}
